b09b06b9ca1e6d2ab38e3b504680cbb94bcdc3e3
Pier2208 <p.leroy.mtl@gmail.com>
2022-04-28 15:47:41 -0400

    Update lazy.directive.ts

--- src/app/directives/lazy.directive.ts
+++ src/app/directives/lazy.directive.ts
@@ -4,28 +4,27 @@ import { AfterViewInit, Directive, ElementRef } from '@angular/core';
   selector: '[lazy]',
 })
 export class LazyDirective implements AfterViewInit {
-  constructor(private el: ElementRef<HTMLImageElement>) {}
+  constructor(private el: ElementRef<HTMLImageElement>) { }
 
   ngAfterViewInit(): void {
     const observer = new IntersectionObserver(
       (entries) => {
         // each entry corresponds to an image
         entries.forEach((entry) => {
-          // when image is visible in the viewport + rootMargin
+          // when image is visible in the viewport
           if (entry.intersectionRatio > 0 || entry.isIntersecting) {
-            console.log('entering viewport', this.el.nativeElement);
-            
-            observer.unobserve(this.el.nativeElement);
+            const src = this.el.nativeElement.getAttribute('data-src')
+            if (src)
+              this.el.nativeElement.setAttribute('src', src)
+
+            const srcset = this.el.nativeElement.getAttribute('data-srcset')
+            if (srcset)
+              this.el.nativeElement.setAttribute('srcset', srcset)
           }
         });
-      },
-      {
-        threshold: 0.01,
-        rootMargin: '75%',
       }
     );
-    // we tell the observer to watch for this HTML DOM element that we set with
-    // the callabck ref earlier
+
     observer.observe(this.el.nativeElement);
   }
 }
